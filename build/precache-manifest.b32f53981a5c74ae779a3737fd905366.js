ba8fbc4b505723b27b98397584bcd79b5b4b200e
Divakar Yadav <divakaryadav125@gmail.com>
2020-01-22 21:13:31 +0530

    build

---
+++ build/precache-manifest.b32f53981a5c74ae779a3737fd905366.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678a8102bfa8f56b1131234c0155794c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c0e789360d9a32e3ac",
+    "url": "/static/css/main.d89035c5.chunk.css"
+  },
+  {
+    "revision": "d6133c05a016d2847d0a",
+    "url": "/static/js/2.772f9377.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.772f9377.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4c0e789360d9a32e3ac",
+    "url": "/static/js/main.d608c74c.chunk.js"
+  },
+  {
+    "revision": "ab758740732ff8feba1d",
+    "url": "/static/js/runtime-main.749b35d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
